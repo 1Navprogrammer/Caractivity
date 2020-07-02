8a281b1739d2e0f04dadbeef16f0a5002bf1c79f
Navpreet Kaur <NK028@atcltd.atc.org.nz>
2020-07-03 08:39:48 +1200

    Add project files.

---
+++ Caractivity/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Caractivity
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.BMWPicture = new System.Windows.Forms.PictureBox();
+            this.ToyotaPicture = new System.Windows.Forms.PictureBox();
+            this.MercedesPicture = new System.Windows.Forms.PictureBox();
+            this.LexusPicture = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.BMWPicture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ToyotaPicture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MercedesPicture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LexusPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(682, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 29);
+            this.button1.TabIndex = 4;
+            this.button1.Text = " Button";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // BMWPicture
+            // 
+            this.BMWPicture.Image = global::Caractivity.Resource1.BMW;
+            this.BMWPicture.Location = new System.Drawing.Point(188, 12);
+            this.BMWPicture.Name = "BMWPicture";
+            this.BMWPicture.Size = new System.Drawing.Size(93, 63);
+            this.BMWPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.BMWPicture.TabIndex = 3;
+            this.BMWPicture.TabStop = false;
+            // 
+            // ToyotaPicture
+            // 
+            this.ToyotaPicture.Image = global::Caractivity.Resource1.Toyota;
+            this.ToyotaPicture.Location = new System.Drawing.Point(363, 12);
+            this.ToyotaPicture.Name = "ToyotaPicture";
+            this.ToyotaPicture.Size = new System.Drawing.Size(93, 63);
+            this.ToyotaPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ToyotaPicture.TabIndex = 2;
+            this.ToyotaPicture.TabStop = false;
+            // 
+            // MercedesPicture
+            // 
+            this.MercedesPicture.Image = global::Caractivity.Resource1.Mercedes;
+            this.MercedesPicture.Location = new System.Drawing.Point(542, 12);
+            this.MercedesPicture.Name = "MercedesPicture";
+            this.MercedesPicture.Size = new System.Drawing.Size(93, 63);
+            this.MercedesPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.MercedesPicture.TabIndex = 1;
+            this.MercedesPicture.TabStop = false;
+            // 
+            // LexusPicture
+            // 
+            this.LexusPicture.Image = global::Caractivity.Resource1.Lexus;
+            this.LexusPicture.Location = new System.Drawing.Point(12, 12);
+            this.LexusPicture.Name = "LexusPicture";
+            this.LexusPicture.Size = new System.Drawing.Size(93, 63);
+            this.LexusPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.LexusPicture.TabIndex = 0;
+            this.LexusPicture.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.BMWPicture);
+            this.Controls.Add(this.ToyotaPicture);
+            this.Controls.Add(this.MercedesPicture);
+            this.Controls.Add(this.LexusPicture);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.BMWPicture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ToyotaPicture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MercedesPicture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LexusPicture)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox LexusPicture;
+        private System.Windows.Forms.PictureBox MercedesPicture;
+        private System.Windows.Forms.PictureBox ToyotaPicture;
+        private System.Windows.Forms.PictureBox BMWPicture;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Caractivity/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Caractivity
+{
+    public partial class Form1 : Form
+    {
+        Lexus Le;
+        BMW Bm;
+        Toyota To;
+        Mercedes Me;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Le = new Lexus("LexusName", LexusPicture);
+            Bm = new BMW("BMWName", BMWPicture);
+            To = new Toyota("ToyotaName", ToyotaPicture);
+            Me = new Mercedes("MercedesName", MercedesPicture);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            if (Le.Picture.Location.Y < 600 &&
+               Bm.Picture.Location.Y< 600 &&
+                To.Picture.Location.Y< 600 &&
+                Me.Picture.Location.Y < 600)
+            {
+                Le.Move(rand.Next(2, 20));
+                Bm.Move(rand.Next(2, 20));
+                To.Move(rand.Next(2, 20));
+                Me.Move(rand.Next(2, 20));
+            }
+
+            else
+            {
+                string winner = "";
+
+                for (int i = 0; i < 4; i++)
+                {
+                    if (Le.Picture.Location.Y >= 600)
+                    {
+                        winner = Le.Name;
+                    }
+                    else if (Le.Picture.Location.Y >= 600)
+                    {
+                        winner = Bm.Name;
+                    }
+                    else if (To.Picture.Location.Y >= 600)
+                    {
+                        winner = To.Name;
+                    }
+                    else if (Me.Picture.Location.Y >= 600)
+                    {
+                        winner = Me.Name;
+                    }
+                    else
+                    {
+                        winner = "Something wents wrong!";
+                    }
+
+                }
+                MessageBox.Show("The race is finished and the winner is" + winner);
+            }
+        }
+    }
+}
+
+       
+    
+
